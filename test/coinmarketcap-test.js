99279070cc28d72c6b62f51aa969ff5fee53124c
q3th3r <q3th3r@gmail.com>
2017-06-17 17:13:48 -0400

    Refactor tests with common output() callback

--- test/coinmarketcap-test.js
+++ test/coinmarketcap-test.js
@@ -1,39 +1,15 @@
 // import {getTicker, getTickers} from '../src/coinmarketcap'
 const {getTicker, getTickers, getGlobalMarketData} = require('../src/coinmarketcap') // Should we use Babel and ES6 imports?
 
-getTickers(function(err, results) {
-    process.stdout.write(results);
-    //console.log(results.toString());
-});
+var output = function(err, result) {
+    if (err) return console.log(err);
+    // process.stdout.write(result);
+    console.log('RES', result.toString());
+};
 
-if (1) {
-    0 && getTicker('ethereum', function(err, result) {
-        if (err) return console.log(err);
-
-        console.log('RES', result.toString());
-    });
-
-    0 && getTicker('ethereum', 'CAD', function(err, result) {
-        if (err) return console.log(err);
-        // process.stdout.write(result);
-        console.log('RES', result.toString());
-    });
-
-    0 && getTicker('ethereum', 'KKIISS', function(err, result) {
-        if (err) return console.log(err);
-        // process.stdout.write(result);
-        console.log('RES', result.toString());
-    });
-
-    0 && getGlobalMarketData(function(err, result) {
-        if (err) return console.log(err);
-
-        console.log('RES', result.toString());
-    });
-
-    0 && getGlobalMarketData('CAD', function(err, result) {
-        if (err) return console.log(err);
-
-        console.log('RES', result.toString());
-    });
-}
+0 && getTickers(output);
+0 && getTicker('ethereum', output);
+0 && getTicker('ethereum', 'CAD', output);
+0 && getTicker('ethereum', 'KKIISS', output);
+0 && getGlobalMarketData(output);
+0 && getGlobalMarketData('CAD', output);
